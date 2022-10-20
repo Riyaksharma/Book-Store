9a2c3b8941b3aa3f4f6c04cc5b017eb4c67d83ad
Riya Kumari <riyaksharma15@gmail.com>
2022-10-20 16:25:32 +0530

    Setup Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+
+//All Authors routers
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i"); //even if we put jo we will get john
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+//New  Authors
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+//Create Author Route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error occured",
+    });
+  }
+
+  // **** OLD CODE *********
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     res.render("authors/new", {
+  //       author: author,
+  //       errorMessage: "Error occured",
+  //     });
+  //   } else {
+  //     // res.redirect(`authors/${newAuthor.id}`);
+  //     res.redirect(`authors`);
+  //   }
+  // });
+});
+
+module.exports = router;
